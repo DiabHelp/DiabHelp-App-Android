8cfc28ce67cd0db5925f3b19a8913b05cc6905c9
Simon MILHAU <simon.milhau@outlook.com>
2015-10-16 01:44:39 +0800

    feature/catalogue_v2 Commit modif alertdialog

--- Diabhelp/app/src/main/java/fr/diabhelp/diabhelp/Core/CatalogRecyclerAdapter.java
+++ Diabhelp/app/src/main/java/fr/diabhelp/diabhelp/Core/CatalogRecyclerAdapter.java
@@ -41,7 +41,7 @@ public class CatalogRecyclerAdapter extends RecyclerView.Adapter<CatalogRecycler
                 public void onClick(View v) {
                     AlertDialog alertDialog = new AlertDialog.Builder(v.getContext()).create();
                     alertDialog.setTitle("Confirmation");
-                    alertDialog.setMessage("Please confirm the module download");
+                    alertDialog.setMessage("Êtes-vous sûr de vouloir être redirigé vers le playstore ?");
                     alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
